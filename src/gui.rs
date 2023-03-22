7ab2709fdc4e9b5a22c1265d8293d0a6437601c3
Garm <melsom.henrik@gmail.com>
2023-03-22 01:08:59 +0100

    🚧 Work on vdom updates

--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,8 @@
-use bevy::prelude::{App, Plugin};
-use dioxus::prelude::*;
+use bevy::{ecs::system::SystemParam, prelude::*, utils::HashMap};
+use dioxus::{
+    core::{ElementId, Mutation, Mutations},
+    prelude::*,
+};
 
 pub mod dioxus_elements {
     pub struct node;
@@ -12,25 +15,121 @@ pub mod dioxus_elements {
     }
 }
 
+#[derive(Resource, Debug)]
+pub struct GuiContext {
+    stack: Vec<Entity>,
+    nodes: HashMap<ElementId, Entity>,
+}
+
+impl FromWorld for GuiContext {
+    fn from_world(world: &mut World) -> Self {
+        let root = world
+            .spawn(NodeBundle {
+                style: Style {
+                    size: Size::width(Val::Percent(100.0)),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .id();
+        let mut nodes = HashMap::new();
+        nodes.insert(ElementId(0), root);
+        Self {
+            stack: vec![],
+            nodes,
+        }
+    }
+}
+
+#[derive(SystemParam)]
+pub struct GuiParams<'w, 's> {
+    pub ctx: ResMut<'w, GuiContext>,
+    pub commands: Commands<'w, 's>,
+}
+
+impl<'w, 's> GuiParams<'w, 's> {
+    pub fn spawn_template_node(&mut self, node: &TemplateNode) -> Entity {
+        use dioxus::core::TemplateNode::*;
+        self.commands.spawn(NodeBundle::default()).id()
+    }
+
+    pub fn apply_mutation(&mut self, templates: &HashMap<&str, Template>, mutation: &Mutation) {
+        use dioxus::core::Mutation::*;
+        match mutation {
+            AppendChildren { id, m } => {
+                let parent = *self.ctx.nodes.get(id).unwrap();
+                for _ in 0..*m {
+                    let child = self.ctx.stack.pop().unwrap();
+                    self.commands.entity(parent).add_child(child);
+                }
+            }
+            AssignId { path, id } => todo!(),
+            CreatePlaceholder { id } => todo!(),
+            CreateTextNode { value, id } => todo!(),
+            HydrateText { path, value, id } => todo!(),
+            LoadTemplate { name, index, id } => {
+                let template = templates.get(name).unwrap();
+                let root = template.roots.get(*index).unwrap();
+                let entity = self.spawn_template_node(root);
+                self.ctx.stack.push(entity);
+                self.ctx.nodes.insert(*id, entity);
+            }
+            ReplaceWith { id, m } => todo!(),
+            ReplacePlaceholder { path, m } => todo!(),
+            InsertAfter { id, m } => todo!(),
+            InsertBefore { id, m } => todo!(),
+            SetAttribute {
+                name,
+                value,
+                id,
+                ns,
+            } => todo!(),
+            SetText { value, id } => todo!(),
+            NewEventListener { name, id } => todo!(),
+            RemoveEventListener { name, id } => todo!(),
+            Remove { id } => todo!(),
+            PushRoot { id } => todo!(),
+        }
+    }
+
+    pub fn apply_mutations(&mut self, mutations: &Mutations) {
+        let templates = mutations
+            .templates
+            .iter()
+            .cloned()
+            .map(|template| (template.name, template))
+            .collect::<HashMap<_, _>>();
+        for mutation in &mutations.edits {
+            self.apply_mutation(&templates, mutation);
+            dbg!(&self.ctx);
+        }
+    }
+}
+
 fn app(cx: Scope) -> Element {
     cx.render(rsx! {
         node {
             background_color: "#ff0000",
-            "hello world"
+            "hello",
+            node {
+                "world"
+            }
         }
     })
 }
 
-fn startup() {
+fn startup(mut gui: GuiParams) {
     let mut vdom = VirtualDom::new(app);
     let mutations = vdom.rebuild();
     dbg!(&mutations);
+    gui.apply_mutations(&mutations);
 }
 
 pub struct GuiPlugin;
 
 impl Plugin for GuiPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(startup);
+        app.init_resource::<GuiContext>()
+            .add_startup_system(startup);
     }
 }
